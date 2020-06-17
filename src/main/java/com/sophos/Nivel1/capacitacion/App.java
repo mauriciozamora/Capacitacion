781c1df92f44dd0d493c872c2ae189372feff949
mauriciozamora <cristian.mzamora1@gmail.com>
2020-06-17 18:56:12 -0500

    primer commit

---
+++ src/main/java/com/sophos/Nivel1/capacitacion/App.java
@@ -0,0 +1,13 @@
+package com.sophos.Nivel1.capacitacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
